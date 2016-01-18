cd73494cd5245e6cbb54057448de567af8d96d02
AlexxxShib <alexxxshib@gmail.com>
2016-01-18 19:52:42 +0600

    init commit

---
+++ commonlibtestapp/src/main/java/com/mobiray/commonlibtestapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mobiray.commonlibtestapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
